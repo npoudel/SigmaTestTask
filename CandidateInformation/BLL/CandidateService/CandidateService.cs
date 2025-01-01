146e416a5b8350d20afaf36fec49f41fa59d6d7e
Nirajan <npoudel@deerhold.com>
2025-01-01 19:40:23 +0545

    added controller and services

---
+++ CandidateInformation/BLL/CandidateService/CandidateService.cs
@@ -0,0 +1,46 @@
+﻿using DAL;
+using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.CandidateService
+{
+    public class CandidateService : ICandidateService
+    {
+        private readonly ICandidateRepository _repository;
+
+        public CandidateService(ICandidateRepository repository)
+        {
+            _repository = repository;
+        }
+
+        /// <summary>
+        /// add or update candidate
+        /// </summary>
+        /// <param name="candidate"></param>
+        public void AddOrUpdateCandidate(Candidate candidate)
+        {
+
+            var candidateInfo = _repository.GetCandidateByEmail(candidate.Email);
+            if (candidateInfo != null)
+            {
+                candidateInfo.FirstName = candidate.FirstName;
+                candidateInfo.LastName = candidate.LastName;
+                candidateInfo.PhoneNumber = candidate.PhoneNumber;
+                candidateInfo.BestCallTime = candidate.BestCallTime;
+                candidateInfo.LinkedInProfile = candidate.LinkedInProfile;
+                candidateInfo.GitHubProfile = candidate.GitHubProfile;
+                candidateInfo.Comment = candidate.Comment;
+                _repository.UpdateCandidate(candidateInfo);
+            }
+            else
+            {
+                _repository.AddCandidate(candidate);
+            }
+            _repository.SaveChanges();
+        }
+    }
+}
---
+++ CandidateInformation/CandidateInformation/Controllers/CandidatesController.cs
@@ -0,0 +1,56 @@
+﻿using BLL;
+using Domain.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace CandidateInformation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CandidatesController : ControllerBase
+    {
+        private readonly ICandidateService _candidateService;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="candidateService"></param>        
+        public CandidatesController(ICandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("[action]", Name = "Index")]
+        public IActionResult Index()
+        {
+            return Ok(new { Message = "API called successfully." });
+        }
+
+        /// <summary>
+        /// add update candidate
+        /// </summary>
+        /// <param name="candidate"></param>
+        /// <returns></returns>
+
+        [HttpPost("[action]",Name = "AddOrUpdateCandidate")]
+        public IActionResult AddOrUpdateCandidate([FromBody] Candidate candidate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                _candidateService.AddOrUpdateCandidate(candidate);
+                return Ok(new { Message = "Candidate saved successfully." });
+            }
+            catch
+            {
+                return BadRequest();
+            }            
+        }
+    }
+}
